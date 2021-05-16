d4f8e3486ad3f36a099ad4c6084066999b4d7c0a
albaysimsek <info@atillakurtulussimsek.com.tr>
2021-05-16 09:09:46 +0300

    Last Release

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow , Menu  } = require('electron')
+let win
 
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true
     }
   })
 
-  win.loadFile('index.html')
+
+win.openDevTools();
+  win.loadURL('https://udemy.com/');
+  win.on('closed', () => {
+    win = null
+  });
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready', ()=>{   
+  createWindow();
+});
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
   }
 })
